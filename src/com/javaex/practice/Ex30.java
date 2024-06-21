267ab32f4293ece56dc9ebbbc79683fba4641b6d
805-6 <805-6@DESKTOP-960EKSC>
2024-06-21 11:37:17 +0900

    dd

--- src/com/javaex/practice/Ex30.java
+++ src/com/javaex/practice/Ex30.java
@@ -1,9 +1,23 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex30 {
 
 	public static void main(String[] args) {
 
+		// 1mile = 1.609
+		
+		System.out.print("마일을 입력하세요: ");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double mile = sc.nextDouble();
+		//System.out.print("
+		
+		
+		sc.close();
+		
 		
 		
 	}
